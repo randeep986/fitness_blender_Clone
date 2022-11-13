0cc6973d8377c3c75946c5b2fe3a85cc946e5180
Randeep Singh <randeepsingh986@gmail.com>
2022-11-13 23:56:22 +0530

    Added Final Submission

--- fitness/src/index.js
+++ fitness/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+
+import { ChakraProvider } from '@chakra-ui/react'
 import Authcontextprovider from './Context/Authcontextprovider';
 import CartContext from './Context/CartContext';
 
